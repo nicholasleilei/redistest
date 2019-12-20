48da2098039bbe3197309a1b9c1d8f48f12b66ba
2587337887 <The Shy@DESKTOP-SQV4IUC>
2019-12-20 18:31:25 +0800

    ===

--- redistest/src/main/java/redistest/TestDemo.java
+++ redistest/src/main/java/redistest/TestDemo.java
@@ -7,6 +7,7 @@ public class TestDemo {
       System.out.println("hello123");
       System.out.println("nieyunshuai¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡££¬£¬£¬..1231288883");
       System.out.println("lvhu");
+   System.out.println("+++++++++++");
       
 	}
 
